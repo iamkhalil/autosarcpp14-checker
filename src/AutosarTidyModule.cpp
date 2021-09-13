8ed801498d9b6c27c1c7c09038bcd569484b0f30
KhouloudMansour <63551598+KhouloudMansour@users.noreply.github.com>
2021-09-13 22:55:28 +0100

    Add checker for rule A12-4-1

--- src/AutosarTidyModule.cpp
+++ src/AutosarTidyModule.cpp
@@ -19,6 +19,17 @@
 #include "Rulem1804Check.h"
 #include "Rulem1805Check.h"
 #include "Rulem4102Check.h"
+#include "Rulea042Check.h"
+#include "Rulea1101Check.h"
+#include "Rulea1241Check.h"
+#include "Rulea1323Check.h"
+#include "Rulea1621Check.h"
+#include "Rulea2133Check.h"
+#include "Rulea312Check.h"
+#include "Rulea313Check.h"
+#include "Rulea314Check.h"
+#include "Rulea391Check.h"
+#include "Rulem1101Check.h"
 
 namespace clang {
 namespace tidy {
@@ -46,6 +57,28 @@ public:
         "autosar-ruleM18-0-5");
     CheckFactories.registerCheck<Rulem4102Check>(
         "autosar-ruleM4-10-2");
+    CheckFactories.registerCheck<Rulea042Check>(
+        "autosar-ruleA0-4-2");
+    CheckFactories.registerCheck<Rulea1101Check>(
+        "autosar-ruleA11-0-1");
+    CheckFactories.registerCheck<Rulea1323Check>(
+        "autosar-ruleA13-2-3");
+    CheckFactories.registerCheck<Rulea1621Check>(
+        "autosar-ruleA16-2-1");
+    CheckFactories.registerCheck<Rulea2133Check>(
+        "autosar-ruleA2-13-3");
+    CheckFactories.registerCheck<Rulea312Check>(
+        "autosar-ruleA3-1-2");
+    CheckFactories.registerCheck<Rulea313Check>(
+        "autosar-ruleA3-1-3");
+    CheckFactories.registerCheck<Rulea314Check>(
+        "autosar-ruleA3-1-4");
+    CheckFactories.registerCheck<Rulea391Check>(
+        "autosar-ruleA3-9-1");
+    CheckFactories.registerCheck<Rulem1101Check>(
+        "autosar-ruleM11-0-1");
+    CheckFactories.registerCheck<Rulea1241Check>(
+        "autosar-ruleA12-4-1");
   }
 };
 
